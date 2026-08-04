e66f4c9128e8bc62cacbda2267d26f712e044ffe
Chaity <chaitymetrouni@gmail.com>
2026-08-04 23:10:41 +0600

    Product System

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,27 +1,46 @@
-const Product = require(`../models/Product`);
+const Product = require("../models/Product");
 
+// show all products
+const getProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
- //GET all products
- const getProducts = async (req, res) => {
-    try {
-        const products = await Product.find();
-        res.status(200).json(products);
-    }catch (error) {
-        res.status(500).json({ message: error.message });
+// show product with id
+const getProductById = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+
+    if (!product) {
+      return res.status(404).json({
+        message: "Product not found",
+      });
     }
- };
 
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(400).json({
+      message: "Invalid product ID",
+    });
+  }
+};
 
+// create new product
+const createProduct = async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(201).json(product);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
 
- //POST a new product
- const createProduct = async (req, res) => {
-    try {
-        const product = await Product.create(req.body);
-        res.status(201).json(product);
-    } catch (error) {
-        res.status(400).json({ message: error.message });
-    }
- };
-
-
- module.exports = { getProducts, createProduct};
+module.exports = {
+  getProducts,
+  getProductById,
+  createProduct,
+};
--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,8 +1,14 @@
-const express = require(`express`);
+const express = require("express");
 const router = express.Router();
-const { getProducts, createProduct } = require(`../controllers/productController`);
 
-router.get(`/`, getProducts);
-router.post(`/`, createProduct);
+const {
+  getProducts,
+  getProductById,
+  createProduct,
+} = require("../controllers/productController");
+
+router.get("/", getProducts);
+router.get("/:id", getProductById);
+router.post("/", createProduct);
 
 module.exports = router;
