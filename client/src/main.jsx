c31df5f992fce4e3b7661f64841c54477992be29
Chaity <chaitymetrouni@gmail.com>
2026-08-05 17:45:10 +0600

    Logo Added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './footer-panels.css'
+import './premium-home.css'
+import './brand-logo.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
